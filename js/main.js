f94c9034f1bef465b5ae38007696d50aaad9c306
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-15 17:34:30 -0400

    Fixed errors.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var $input = $('#colour-picker');
 var $ball = $('.ball');
 var $form = $('form');
 
-  $input.on('change', function() {
-    $ball.css('background-color', $input.val());
-  });
+$input.on('change', function () {
+  $ball.css('background-color', $input.val());
+});
